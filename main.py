4233e4470bcd84451966a3cb1a93a606da545613
Void <109233052+V-o-id@users.noreply.github.com>
2023-11-21 14:23:43 +0100

    Added basic functionality for cloning a repository and saving the days and time of commits in a txt.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import subprocess
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def main():
+    # Replace these variables with the actual owner and name of the repository
+    repository_url = 'https://github.com/freeCodeCamp/freeCodeCamp.git'
+    repository_name = repository_url.split('/')[-1].split('.')[0]
+
+    # Check if the repository directory exists
+    if not os.path.exists(repository_name):
+        # Clone the repository
+        subprocess.run(['git', 'clone', repository_url])
+    else:
+        # If the repository already exists, pull the latest changes
+        subprocess.run(['git', 'reset', '--hard'], cwd=repository_name)
+        subprocess.run(['git', 'pull'], cwd=repository_name)
+
+    # Navigate to the cloned repository
+    os.chdir(repository_name)
+
+    # Specify the absolute path for the output file
+    output_file = f'../{repository_name}_logs.txt'
+
+    # Get the commit logs and save to the specified file
+    with open(output_file, 'w') as file:
+        subprocess.run(['git', 'log', '--pretty="%aD"'], stdout=file)
+
+    print(f'Commit logs saved to {output_file}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
